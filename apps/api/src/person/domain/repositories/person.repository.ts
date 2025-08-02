9b953b4f12f08b2dc059980a53bafbdcfa72208a
João Trajano <joao.trajanosouza@gmail.com>
2025-08-02 12:56:10 -0300

    feature(api): created repository for persin

---
+++ apps/api/src/person/domain/repositories/person.repository.ts
@@ -0,0 +1,15 @@
+import { Repository } from '@/shared/infrastructure/database/repository.inteface'
+import { PersonEntity } from '../person.entity'
+
+export abstract class PersonRepository
+  implements Omit<Repository<PersonEntity>, 'fetchAll' | 'fetchById'>
+{
+  abstract create(person: PersonEntity): Promise<PersonEntity>
+  abstract update(person: PersonEntity): Promise<PersonEntity>
+  abstract delete(id: string): Promise<void>
+  abstract fetch(
+    name?: string,
+    email?: string,
+    cpf?: string
+  ): Promise<PersonEntity[]>
+}
