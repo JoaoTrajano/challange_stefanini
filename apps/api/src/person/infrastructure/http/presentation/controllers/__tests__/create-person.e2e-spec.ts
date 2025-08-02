fd7b93a16b71d9da3e9525cadbcae262b27d5f2a
João Trajano <joao.trajanosouza@gmail.com>
2025-08-02 15:20:25 -0300

    tests:(api): created test e2e for controller persons

---
+++ apps/api/src/person/infrastructure/http/presentation/controllers/__tests__/create-person.e2e-spec.ts
@@ -0,0 +1,43 @@
+import { INestApplication, ValidationPipe } from '@nestjs/common'
+import { Test } from '@nestjs/testing'
+import { afterAll, beforeAll, describe } from 'vitest'
+
+import { AppModule } from '@/app.module'
+import { PrismaService } from '@/shared/infrastructure/database/postgres/adapters/prisma/prisma.service'
+
+describe('CreatePersonController e2e test', () => {
+  let app: INestApplication
+  let prismaService: PrismaService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
+
+    prismaService = moduleRef.get(PrismaService)
+
+    await app.init()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  describe('POST /persons', () => {
+    // test('should be able return a entity person created', async () => {
+    //   const res = await request(app.getHttpServer()).post('/persons').send({
+    //     title: 'New person',
+    //   })
+    //   const hasPerson = await prismaService.person.findFirst({
+    //     where: {
+    //       title: 'New person',
+    //     },
+    //   })
+    //   expect(res.statusCode).toBe(201)
+    //   expect(hasPerson).toBeTruthy()
+    // })
+  })
+})
