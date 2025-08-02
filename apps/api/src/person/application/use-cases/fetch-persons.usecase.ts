d750dbc009948821e87c303b8e3d6d97bcf43c14
João Trajano <joao.trajanosouza@gmail.com>
2025-08-02 14:05:58 -0300

    feature(api): create a new use case for fetch persons

---
+++ apps/api/src/person/application/use-cases/fetch-persons.usecase.ts
@@ -0,0 +1,35 @@
+import { PersonEntity } from '@/person/domain/person.entity'
+import { PersonRepository } from '@/person/domain/repositories/person.repository'
+import { UseCase } from '@/shared/application/use-cases/use-case.interface'
+import { Either, rigth } from '@/shared/errors/either'
+
+export type FetchPersonUseCaseInput = {
+  name?: string
+  email?: string
+  document?: string
+}
+
+export type FetchPersonUseCaseOutput = Either<
+  unknown,
+  {
+    person: PersonEntity[]
+  }
+>
+
+export class FetchPersonUseCase
+  implements UseCase<FetchPersonUseCaseInput, FetchPersonUseCaseOutput>
+{
+  constructor(private readonly personRepository: PersonRepository) {}
+
+  async execute(
+    input: FetchPersonUseCaseInput
+  ): Promise<FetchPersonUseCaseOutput> {
+    const person = await this.personRepository.fetch(
+      input.name,
+      input.email,
+      input.document
+    )
+
+    return rigth({ person })
+  }
+}
