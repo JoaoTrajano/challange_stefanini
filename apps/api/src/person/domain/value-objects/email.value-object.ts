0d3dff3faffb3184cdf571181a6ae28b66ceb761
João Trajano <joao.trajanosouza@gmail.com>
2025-08-02 13:05:21 -0300

    feature(api): crated a new value object for email

---
+++ apps/api/src/person/domain/value-objects/email.value-object.ts
@@ -0,0 +1,32 @@
+export class Email {
+  private readonly value: string
+
+  constructor(email: string) {
+    const normalized = this.normalize(email)
+
+    if (!this.isValid(normalized)) throw new Error('E-mail is not valid')
+
+    this.value = normalized
+  }
+
+  private normalize(email: string): string {
+    return email.trim().toLowerCase()
+  }
+
+  private isValid(email: string): boolean {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    return emailRegex.test(email)
+  }
+
+  public getValue(): string {
+    return this.value
+  }
+
+  public equals(other: Email): boolean {
+    return this.value === other.getValue()
+  }
+
+  public toString(): string {
+    return this.value
+  }
+}
