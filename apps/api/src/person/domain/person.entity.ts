abf0633bc140915502f6527e12ff09d70ef2418c
João Trajano <joao.trajanosouza@gmail.com>
2025-08-02 12:17:14 -0300

    feature(api): created the props of the entity person

--- apps/api/src/person/domain/person.entity.ts
+++ apps/api/src/person/domain/person.entity.ts
@@ -1,13 +1,55 @@
 import { Entity } from '@/shared/domain/entity'
+import { Optional } from '@/shared/types/optional'
 
 type PersonEntityProps = {
   cpf: string
   name: string
   birthDate: Date
+  gender?: string
+  email?: string
+  birthplace?: string
+  nationality?: string
 }
 
 export class PersonEntity extends Entity<PersonEntityProps> {
   constructor(props: PersonEntityProps) {
     super(props)
+  }
+
+  get cpf(): string {
+    return this.props.cpf
+  }
+
+  get name(): string {
+    return this.props.name
+  }
+
+  get birthDate(): Date {
+    return this.props.birthDate
+  }
+
+  get gender(): string | undefined {
+    return this.props.gender
+  }
+
+  get email(): string | undefined {
+    return this.props.email
+  }
+
+  get birthplace(): string | undefined {
+    return this.props.birthplace
+  }
+
+  get nationality(): string | undefined {
+    return this.props.nationality
+  }
+
+  static create(
+    props: Optional<
+      PersonEntityProps,
+      'gender' | 'birthplace' | 'email' | 'nationality'
+    >
+  ): PersonEntity {
+    return new PersonEntity({ ...props })
   }
 }
