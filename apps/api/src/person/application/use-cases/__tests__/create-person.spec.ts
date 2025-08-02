c012f5ddd4d04aa18403e47487024a584144cd96
João Trajano <joao.trajanosouza@gmail.com>
2025-08-02 13:32:05 -0300

    feature(api): create a new use case for create person and unit test for use case

---
+++ apps/api/src/person/application/use-cases/__tests__/create-person.spec.ts
@@ -0,0 +1,60 @@
+import { Document } from '@/person/domain/value-objects/document.value-object'
+import { PersonInMemoryRepository } from '@/person/infrastructure/database/in-memory/repositories/task-in-memory-repository'
+import { MissingFieldError } from '../../errors/missing-field-error'
+import { CreatePersonUseCase } from '../create-person.usecase'
+
+let sut: CreatePersonUseCase
+let repo: PersonInMemoryRepository
+
+beforeAll(() => {
+  repo = new PersonInMemoryRepository()
+  sut = new CreatePersonUseCase(repo)
+})
+
+describe('CreatePersonUseCase Unit Test', () => {
+  it('should be able create a person with correct properties', async () => {
+    const result = await sut.execute({
+      birthDate: new Date(),
+      document: new Document('575.359.890-04'),
+      name: 'Jhon Doe',
+    })
+
+    expect(result.isRight()).toBe(true)
+
+    if (result.isRight()) {
+      const { person } = result.value
+      expect(person).toHaveProperty('id')
+      expect(person.createdAt).toBeInstanceOf(Date)
+    }
+  })
+
+  it('should throw an error if the name is empty', async () => {
+    const result = await sut.execute({
+      name: '',
+      birthDate: new Date(),
+      document: new Document('575.359.890-04'),
+    })
+    expect(result.isLeft()).toBeTruthy()
+    expect(result.value).toBeInstanceOf(MissingFieldError)
+  })
+
+  it('should throw an error if the birthDate is empty', async () => {
+    const result = await sut.execute({
+      name: 'Jhon Doe',
+      birthDate: null,
+      document: new Document('575.359.890-04'),
+    })
+    expect(result.isLeft()).toBeTruthy()
+    expect(result.value).toBeInstanceOf(MissingFieldError)
+  })
+
+  it('should throw an error if the document is empty', async () => {
+    const result = await sut.execute({
+      name: 'Jhon Doe',
+      birthDate: new Date(),
+      document: null,
+    })
+    expect(result.isLeft()).toBeTruthy()
+    expect(result.value).toBeInstanceOf(MissingFieldError)
+  })
+})
--- apps/api/src/person/application/use-cases/create-person.usecase.ts
+++ apps/api/src/person/application/use-cases/create-person.usecase.ts
@@ -1,1 +1,44 @@
+import { PersonEntity } from '@/person/domain/person.entity'
+import { PersonRepository } from '@/person/domain/repositories/person.repository'
+import { Document } from '@/person/domain/value-objects/document.value-object'
+import { Email } from '@/person/domain/value-objects/email.value-object'
+import { UseCase } from '@/shared/application/use-cases/use-case.interface'
+import { Either, left, rigth } from '@/shared/errors/either'
+import { MissingFieldError } from '../errors/missing-field-error'
 
+type CreatePersonUseCaseInput = {
+  name: string
+  document: Document
+  birthDate: Date
+  gender?: string
+  email?: Email
+  birthplace?: string
+  nationality?: string
+}
+
+type CreatePersonUseCaseOutput = Either<
+  MissingFieldError,
+  { person: PersonEntity }
+>
+
+export class CreatePersonUseCase
+  implements UseCase<CreatePersonUseCaseInput, CreatePersonUseCaseOutput>
+{
+  constructor(private readonly personRepository: PersonRepository) {}
+
+  async execute(
+    input: CreatePersonUseCaseInput
+  ): Promise<CreatePersonUseCaseOutput> {
+    if (!input.name) return left(new MissingFieldError('Name is required'))
+    if (!input.document)
+      return left(new MissingFieldError('Document is required'))
+    if (!input.birthDate)
+      return left(new MissingFieldError('Birth Date is required'))
+
+    const personCreated = await this.personRepository.create(
+      PersonEntity.create({ ...input })
+    )
+
+    return rigth({ person: personCreated })
+  }
+}
